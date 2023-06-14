6abfff3e772f3928e419c47d0855daf4960422c3
MuhammedGhazi <muhamedghazi79@gmail.com>
2023-06-15 00:38:47 +0300

    newest books cubit done

--- lib/features/home/presentation/maniger/newset_books_cubit/newset_books_cubit.dart
+++ lib/features/home/presentation/maniger/newset_books_cubit/newset_books_cubit.dart
@@ -1,9 +1,18 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 import 'package:my_bookly/features/home/data/models/book_model/book_model.dart';
+import 'package:my_bookly/features/home/data/repos/home_repos.dart';
 
 part 'newset_books_state.dart';
 
 class NewsetBooksCubit extends Cubit<NewsetBooksState> {
-  NewsetBooksCubit() : super(NewsetBooksInitial());
+  NewsetBooksCubit(this.homeRepo) : super(NewsetBooksInitial());
+  final HomeRepo homeRepo;
+
+  Future<void> fetchNewestdBooks() async {
+    emit(NewsetBooksLoading());
+    var result = await homeRepo.fetchFeaturedBooks();
+    result.fold((failure) => {emit(NewsetBooksFailure(failure.errMessage))},
+        (books) => {emit(NewsetBooksSuccess(books))});
+  }
 }
